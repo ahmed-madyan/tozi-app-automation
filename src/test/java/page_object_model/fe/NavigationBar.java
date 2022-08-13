da76abb89f611103848a9abc401209a1db2f0df9
Ahmed Madyan <ahmed.madyan@vodafone.com>
2022-08-13 04:12:44 +0200

    Create NavigationBar.java

---
+++ src/test/java/page_object_model/fe/NavigationBar.java
@@ -0,0 +1,21 @@
+package page_object_model.fe;
+
+import io.appium.java_client.AppiumBy;
+
+public class NavigationBar {
+    private static final AppiumBy cosmos_Button = (AppiumBy) AppiumBy.accessibilityId("Cosmos_Button");
+    private static final AppiumBy mySpace_Button = (AppiumBy) AppiumBy.accessibilityId("MySpace_Button");
+    private static final AppiumBy burgerMenu_Button = (AppiumBy) AppiumBy.accessibilityId("Drawer_BurgerIcon");
+
+    public void open_Cosmos_Tab() {
+
+    }
+
+    public void open_MySpace_Tab() {
+
+    }
+
+    public void open_Burger_Menu() {
+
+    }
+}
